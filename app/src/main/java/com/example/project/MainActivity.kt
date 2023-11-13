e1a383bf3fe0293a01e3f63a1e47890b8d2a2379
chistyakova <chistyakova@umarsh.ru>
2023-11-13 21:04:29 +0300

    modul-20 Заполнение верхнего меню картинками и создание кнопок

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.project
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnMenu = findViewById<Button>(R.id.button_menu)
+        val btnFavorites = findViewById<Button>(R.id.button_favorites)
+        val btnLater = findViewById<Button>(R.id.button_later)
+        val btnCollections = findViewById<Button>(R.id.button_collections)
+        val btnSettings = findViewById<Button>(R.id.button_settings)
+        btnMenu.setOnClickListener{
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        btnFavorites.setOnClickListener{
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        btnLater.setOnClickListener{
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        btnCollections.setOnClickListener{
+            Toast.makeText(this, "Подборка", Toast.LENGTH_SHORT).show()
+        }
+        btnSettings.setOnClickListener{
+            Toast.makeText(this, "Настройка", Toast.LENGTH_SHORT).show()
+        }
     }
 }
