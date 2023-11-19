c050b6bed1501555e9341e1a7d601216d1d756a3
chistyakova <chistyakova@umarsh.ru>
2023-11-19 12:14:42 +0300

    modul-20 изменение подключенных библиотек

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.project
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
