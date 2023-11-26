2198c12c691cf0eb3401a3a84efe6ab3829a63c9
chistyakova <chistyakova@umarsh.ru>
2023-11-26 17:52:41 +0300

    modul-22 Добавлен GridLayout с четырься постерами. Постеры появляются онимировано. При нажатии на постер происходит разная анимация

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,22 +1,35 @@
 package com.example.project
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.view.animation.AnticipateOvershootInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
         val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val poster2 = findViewById<CardView>(R.id.poster_2)
+        val poster3 = findViewById<CardView>(R.id.poster_3)
+        val poster4 = findViewById<CardView>(R.id.poster_4)
+
         topAppBar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.settings -> {
@@ -44,6 +57,53 @@ class MainActivity : AppCompatActivity() {
                 }
                 else -> false
             }
+        }
+
+        poster2.setOnClickListener {
+            val myAnimation5 = AnimationUtils.loadAnimation(this, R.anim.rotate_scale)
+            poster2.startAnimation(myAnimation5)
+        }
+
+        val animatorPosr = AnimatorSet()
+        val animatorPosr1 = AnimatorSet()
+        poster3.setOnClickListener {
+            val poster3Anim = ObjectAnimator.ofFloat(poster3, View.TRANSLATION_Y, -150f)
+            poster3Anim.interpolator = AnticipateOvershootInterpolator()
+            poster3Anim.duration = 1500
+
+            val poster3Anim1 = ObjectAnimator.ofFloat(poster3, View.TRANSLATION_X, -125f)
+            poster3Anim1.interpolator = AnticipateOvershootInterpolator()
+            poster3Anim1.duration = 1500
+
+            val poster3Anim2 = ObjectAnimator.ofFloat(poster3, View.ALPHA, 1f)
+            poster3Anim2.duration = 1500
+            poster3Anim2.interpolator = AnticipateOvershootInterpolator()
+
+            val poster3Anim3 = ObjectAnimator.ofFloat(poster3, View.SCALE_X, 2f)
+            poster3Anim3.duration = 1500
+
+            animatorPosr.playTogether(poster3Anim1, poster3Anim)
+            animatorPosr.playTogether(poster3Anim2, poster3Anim3)
+            animatorPosr.start()
+            animatorPosr.reverse()
+        }
+
+        poster4.setOnClickListener {
+            val poster3Anim = ObjectAnimator.ofFloat(poster4, View.SCALE_Y, 0.2f)
+            poster3Anim.duration = 1000
+
+            val poster3Anim1 = ObjectAnimator.ofFloat(poster4, View.ALPHA, 0f)
+            poster3Anim1.duration = 1000
+
+            val poster3Anim2 = ObjectAnimator.ofFloat(poster4, View.SCALE_Y, 1f)
+            poster3Anim2.duration = 1500
+
+            val poster3Anim3 = ObjectAnimator.ofFloat(poster4, View.ALPHA, 1f)
+            poster3Anim3.duration = 1500
+
+            animatorPosr1.play(poster3Anim)
+            animatorPosr1.play(poster3Anim2).after(poster3Anim)
+            animatorPosr1.start()
         }
     }
 }
