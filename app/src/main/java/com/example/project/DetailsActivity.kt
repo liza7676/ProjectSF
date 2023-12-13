0ecb6357a6ee441f28674bf41cdc118ccd7c2502
chistyakova <chistyakova@umarsh.ru>
2023-12-13 17:06:10 +0300

    modul-24  создано меню в окне Детали и шейки к кнопкам

--- app/src/main/java/com/example/project/DetailsActivity.kt
+++ app/src/main/java/com/example/project/DetailsActivity.kt
@@ -2,9 +2,16 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +29,38 @@ class DetailsActivity : AppCompatActivity() {
         details_poster.setImageResource(film.poster)
         //Устанавливаем описание
         details_description.text = film.description
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navigation = findViewById<CoordinatorLayout>(R.id.details_id)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            Log.d("YAYAYA", "${it.itemId}")
+            when (it.itemId) {
+                R.id.favorites -> {
+                    val snackbar = Snackbar.make(navigation, "В избранное", Snackbar.LENGTH_SHORT)
+                    snackbar.setAction("Push!!!", ){
+                        Toast.makeText(this, "Посмотреть позже ", Toast.LENGTH_SHORT).show()
+                    }
+                    snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.yello))
+                    snackbar.show()
+                    true
+                }
+                R.id.watch_later -> {
+                    val snackbar = Snackbar.make(navigation, "Посмотреть позже ", Snackbar.LENGTH_SHORT)
+                    snackbar.setAction("Push!!!", ){
+                        Toast.makeText(this, "Посмотреть позже ", Toast.LENGTH_SHORT).show()
+                    }
+                    snackbar.setActionTextColor(ContextCompat.getColor(this, R.color.yello))
+                    snackbar.show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
 }
