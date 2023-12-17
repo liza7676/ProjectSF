5f366f8109303f1ea7be2f7923d806c3ba41e1e5
chistyakova <chistyakova@umarsh.ru>
2023-12-17 19:44:30 +0300

    modul-25 Реализовано: - Оставить одно активити, в котором будет располагаться панель навигации и placeholder для фрагментов. - Главный экран и экран с деталями перевести на фрагменты. - Добавьте всплывающее меню о том, что пользователь сейчас покинет приложение (учитывайте стэк фрагментов).

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -14,37 +14,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var filmsAdapter: FilmListRecyclerAdapter
-    val filmsDataBase = listOf(
-        Film("Унесённые призраками", R.drawable.film1, "Тихиро с мамой и папой переезжает в новый дом. Заблудившись по дороге, они оказываются в странном пустынном городе, где их ждет великолепный пир. Родители с жадностью набрасываются на еду и к ужасу девочки превращаются в свиней, став пленниками злой колдуньи Юбабы. Теперь, оказавшись одна среди волшебных существ и загадочных видений, Тихиро должна придумать, как избавить своих родителей от чар коварной старухи. "),
-        Film("Однажды в… Голливуде", R.drawable.film2, "1969 год, золотой век Голливуда уже закончился. Известный актёр Рик Далтон и его дублер Клифф Бут пытаются найти свое место в стремительно меняющемся мире киноиндустрии. "),
-        Film("Убить Билла", R.drawable.film3, "В беременную наёмную убийцу по кличке Чёрная Мамба во время бракосочетания стреляет человек по имени Билл. Но голова у женщины оказалась крепкой — пролежав четыре года в коме, бывшая невеста приходит в себя. Она горит желанием найти предателей. Теперь только безжалостная месть успокоит сердце Чёрной Мамбы, и она начинает по очереди убивать членов банды Билла, решив оставить главаря напоследок. "),
-        Film("ВАЛЛ·И", R.drawable.film4, "Робот ВАЛЛ·И из года в год прилежно трудится на опустевшей Земле, очищая нашу планету от гор мусора, которые оставили после себя улетевшие в космос люди. Он и не представляет, что совсем скоро произойдут невероятные события, благодаря которым он встретит друзей, поднимется к звездам и даже сумеет изменить к лучшему своих бывших хозяев, совсем позабывших родную Землю. "),
-        Film("Шрэк", R.drawable.film5, "Жил да был в сказочном государстве большой зеленый великан по имени Шрэк. Жил он в гордом одиночестве в лесу, на болоте, которое считал своим. Но однажды злобный коротышка — лорд Фаркуад, правитель волшебного королевства, безжалостно согнал на Шрэково болото всех сказочных обитателей. "),
-        Film("Ночной дозор", R.drawable.film6, "Ночью можно столкнуться с темными магами, вампирами и прочей нечистью. Но по следу «ночных охотников» идет Ночной Дозор. Его предназначение — сохранение равновесия между добром и злом, нарушение которого вызывает разрушения, войны, революции, вселенские катастрофы. "),
-        Film("Форрест Гамп", R.drawable.film7, "Сидя на автобусной остановке, Форрест Гамп — не очень умный, но добрый и открытый парень — рассказывает случайным встречным историю своей необыкновенной жизни." +
-                "С самого малолетства парень страдал от заболевания ног, соседские мальчишки дразнили его, но в один прекрасный день Форрест открыл в себе невероятные способности к бегу. Подруга детства Дженни всегда его поддерживала и защищала, но вскоре дороги их разошлись. "),
-        Film("Достучаться до небес", R.drawable.film8, "Волею судеб две абсолютные противоположности, тихоня Руди и разгильдяй Мартин, оказываются в одной больничной палате. Узнав неутешительные прогнозы, друзья решают использовать последние дни на полную катушку — угнать машину с деньгами, напиться текилы, и, конечно, увидеть море. "),
-        Film("Мой сосед Тоторо", R.drawable.film9, "Сестры Сацуки и Мэй переезжают вместе с папой в деревенский дом. Однажды девочки обнаруживают, что по соседству с ними живут лесные духи — хранители леса во главе со своим могущественным и добрым повелителем Тоторо. Постепенно Тоторо становится другом девочек, помогая им в их повседневных приключениях. "),
-        Film("Пятый элемент", R.drawable.film10, "Каждые пять тысяч лет открываются двери между измерениями и темные силы стремятся нарушить существующую гармонию. Каждые пять тысяч лет Вселенной нужен герой, способный противостоять этому злу. XXIII век. Нью-йоркский таксист Корбен Даллас должен решить глобальную задачу - спасение всего рода человеческого. "),
-    )
+    private var backPress = 0L
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
         val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-
-        topAppBar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.settings -> {
-                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                else -> false
-            }
-        }
 
         bottom_navigation.setOnNavigationItemSelectedListener {
             Log.d("YAYAYA", "${it.itemId}")
@@ -64,39 +41,46 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+        //Запускаем фрагмент при старте
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_placeholder, HomeFragment())
+            .addToBackStack(null)
+            .commit()
 
+    }
 
+    fun launchDetailsFragment(film: Film) {
+        //Создаем "посылку"
+        val bundle = Bundle()
+        //Кладем наш фильм в "посылку"
+        bundle.putParcelable("film", film)
+        //Кладем фрагмент с деталями в перменную
+        val fragment = DetailsFragment()
+        //Прикрепляем нашу "посылку" к фрагменту
+        fragment.arguments = bundle
 
-        val main_recycler = findViewById<RecyclerView>(R.id.main_recycler)
-        main_recycler.apply {
-            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
-            //оставим его пока пустым, он нам понадобится во второй части задания
-            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
-                override fun click(film: Film/*, position: Int*/) {
-                    //Создаем бандл и кладем туда объект с данными фильма
-                    val bundle = Bundle()
-                    //Первым параметром указывается ключ, по которому потом будем искать, вторым сам
-                    //передаваемый объект
-                    bundle.putParcelable("film", film)
-                    //Запускаем наше активити
-                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
-                    //Прикрепляем бандл к интенту
-                    intent.putExtras(bundle)
-                    //Запускаем активити через интент
-                    startActivity(intent)
-                }
-            })
-            //Присваиваем адаптер
-            adapter = filmsAdapter
-            //Присвои layoutmanager
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            //Применяем декоратор для отступов
-            val decorator = TopSpacingItemDecoration(8)
-            addItemDecoration(decorator)
-
+        //Запускаем фрагмент
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+    override fun onBackPressed() { //выход по двойной стрелке назад из программы
+        if (supportFragmentManager.backStackEntryCount == 1){
+            if (backPress + TIME_INTERVAL > System.currentTimeMillis()){
+                super.onBackPressed()
+                finish()
+            } else{
+                Toast.makeText(this, "double tap for exit", Toast.LENGTH_SHORT).show()
+            }
+            backPress = System.currentTimeMillis()
+        } else {
+            super.onBackPressed()
         }
-
-        //Кладем нашу БД в RV
-        filmsAdapter.addItems(filmsDataBase)
+    }
+    companion object{
+        const val TIME_INTERVAL = 2000
     }
 }
